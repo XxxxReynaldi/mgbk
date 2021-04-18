0d39ad69513b29c7106d68c0ce92da2b63ad36e5
XxxxReynaldi <bima.reynaldi09@gmail.com>
2021-04-19 02:05:30 +0700

    Update View Harian Printt

--- resources/views/laporan/print/harian.blade.php
+++ resources/views/laporan/print/harian.blade.php
@@ -15,66 +15,59 @@
 <body>
 
     <main>
-        
         <div class="main-content remove-last-child">
             
             <table class="border w-100 border-collapse">
+                <tr>
+                    <th style="width: 10px;" class="text-align-left border-right border-bottom p-min">No</th>
+                    <th style="width: 30%;" class="border-right border-bottom p-min">Jenis Kegiatan</th>
+                    <th style="width: 40%;" class="border-right border-bottom p-min">Detail</th>
+                    <th class="border-right border-bottom p-min">Jumlah Kegiatan</th>
+                    <th class="border-bottom p-min">Ekuivalensi Jam</th>
+                </tr>
+
+                @if (count($reports) == 0)
+                    <tr>
+                        <td>No data found</td>
+                    </tr>
+                @else
+                    @php
+                        $totalJam = 0;
+                    @endphp
+                    @foreach($reports as $report)
+                    @php
+                        // $totalJam += $report->kegiatan()->sum('ekuivalen');
+                        $totalJam += $report->jumlah_ekuivalen;
+                    @endphp
+                    <tr>
+                        {{-- <td class="text-align-center border-right border-bottom p-min">{{ $loop->iteration }}</td>
+                        <td class="text-align-left border-right border-bottom p-min">{{ $report->kegiatan->kegiatan }}</td>
+                        <td class="text-align-justify border-right border-bottom p-min">{{ $report->detail }}</td>
+                        <td class="text-align-center border-right border-bottom p-min">{{ $report->kegiatan()->count() }}</td>
+                        <td class="text-align-center border-right border-bottom p-min">{{ $report->kegiatan()->sum('ekuivalen') }}</td> --}}
+
+                        <td class="text-align-center box-decoration-break border-right border-top border-bottom p-min">{{ $loop->iteration }}</td>
+                        <td class="text-align-left box-decoration-break border-right border-top border-bottom p-min">{{ $report->kegiatan }}</td>
+                        <td class="text-align-justify box-decoration-break border-right border-top border-bottom p-min">{{ $report->detail }}</td>
+                        <td class="text-align-center box-decoration-break border-right border-top border-bottom p-min">{{ $report->jumlah_kegiatan }}</td>
+                        <td class="text-align-center box-decoration-break border-right border-top border-bottom p-min">{{ $report->jumlah_ekuivalen }}</td>
+                    </tr>
+                    @endforeach
 
                     <tr>
-                        <th style="width: 10px;" class="text-align-left border-right border-bottom p-min">No</th>
-                        <th style="width: 30%;" class="border-right border-bottom p-min">Jenis Kegiatan</th>
-                        <th style="width: 40%;" class="border-right border-bottom p-min">Detail</th>
-                        <th class="border-right border-bottom p-min">Jumlah Kegiatan</th>
-                        <th class="border-bottom p-min">Ekuivalensi Jam</th>
+                        <th colspan="4" class="border-right border-bottom p-min">
+                            Total Jam
+                        </th>
+                        <td class="text-align-center border-right border-bottom p-min">
+                            {{ $totalJam }}
+                        </td>
                     </tr>
-
-                    @if (count($reports) == 0)
-                        <tr>
-                            <td>No data found</td>
-                        </tr>
-                    @else
-                        @php
-                            $totalJam = 0;
-                        @endphp
-                        @foreach($reports as $report)
-                        @php
-                            // $totalJam += $report->kegiatan()->sum('ekuivalen');
-                            $totalJam += $report->jumlah_ekuivalen;
-                        @endphp
-                        <tr>
-                            {{-- <td class="text-align-center border-right border-bottom p-min">{{ $loop->iteration }}</td>
-                            <td class="text-align-left border-right border-bottom p-min">{{ $report->kegiatan->kegiatan }}</td>
-                            <td class="text-align-justify border-right border-bottom p-min">{{ $report->detail }}</td>
-                            <td class="text-align-center border-right border-bottom p-min">{{ $report->kegiatan()->count() }}</td>
-                            <td class="text-align-center border-right border-bottom p-min">{{ $report->kegiatan()->sum('ekuivalen') }}</td> --}}
-
-                            <td class="text-align-center box-decoration-break border-right border-top border-bottom p-min">{{ $loop->iteration }}</td>
-                            <td class="text-align-left box-decoration-break border-right border-top border-bottom p-min">{{ $report->kegiatan }}</td>
-                            <td class="text-align-justify box-decoration-break border-right border-top border-bottom p-min">{{ $report->detail }}</td>
-                            <td class="text-align-center box-decoration-break border-right border-top border-bottom p-min">{{ $report->jumlah_kegiatan }}</td>
-                            <td class="text-align-center box-decoration-break border-right border-top border-bottom p-min">{{ $report->jumlah_ekuivalen }}</td>
-                        </tr>
-                        @endforeach
-
-                        <tr>
-                            <th colspan="4" class="border-right border-bottom p-min">
-                                Total Jam
-                            </th>
-                            <td class="text-align-center border-right border-bottom p-min">
-                                {{ $totalJam }}
-                            </td>
-                        </tr>
-                    @endif
-
+                @endif
             </table>
-            
 
-        </div>
-        
+        </div>    
     </main>
 
-    
-    
 </body>
 
 </html>
